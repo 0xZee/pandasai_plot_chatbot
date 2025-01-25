634d9fbd31614bae3e4664e51696ef01fcad511c
0xZee <91375714+0xZee@users.noreply.github.com>
2025-01-25 22:25:42 +0100

    Update data.py

--- data.py
+++ data.py
@@ -5,10 +5,8 @@ import streamlit as st
 @st.cache_data
 def get_stock_data():
     # Example usage
-    tickers = ["AAPL","GOOGL", "MSFT","ORCL", "NOW","SNOW", "PLTR","IONQ", "NVDA", "ASTS", "RKLB", "QBTS", "RXRX", "AMD", "LAES", "GSAT", "LUNR"]
-    list_keys = ['symbol', 'currentPrice','marketCap', 'trailingPE', 'forwardPE', 
-                'priceToSalesTrailing12Months', 'shortRatio', 'enterpriseToRevenue', 
-                'enterpriseToEbitda', 'targetHighPrice', 'returnOnEquity']
+    tickers = ['AAPL', 'NVDA', 'MSFT', 'GOOGL', 'AMZN', 'META', 'TSLA', 'AVGO', 'WMT', 'LLY', 'ORCL', 'COST', 'NFLX', 'NVO', 'CRM', 'KO', 'CSCO', 'NOW', 'IBM', 'BABA', 'GE', 'AMD', 'DIS', 'ADBE', 'QCOM', 'PLTR', 'RTX', 'ARM', 'ANET', 'NEE', 'HON', 'UBER', 'SHOP', 'MU', 'PANW', 'APP', 'MRVL', 'BYDDY', 'NKE', 'CEG', 'SPOT', 'MSTR', 'PYPL', 'INTC', 'CRWD', 'COIN', 'FTNT', 'WDAY', 'JD', 'TTD', 'VST', 'SNOW', 'SQ', 'DDOG', 'HOOD', 'NET', 'RDDT', 'ZS', 'IOT', 'TER', 'PSTG', 'ALAB', 'MDB', 'GRAB', 'SMCI', 'AFRM', 'SOFI', 'NTNX', 'CYBR', 'OKTA', 'RIVN', 'XPEV', 'RBRK', 'MNDY', 'AUR', 'ROKU', 'RKLB', 'ONTO', 'GTLB', 'PSN', 'EXAS', 'LCID', 'NIO', 'ENPH', 'IONQ', 'AES', 'PATH', 'S', 'ASTS', 'LYFT', 'TEM', 'BE', 'SOUN', 'KTOS', 'AVAV', 'PONY', 'BEPC', 'BRZE', 'ACHR', 'AI', 'VKTX', 'OKLO', 'GSAT', 'CRSP', 'RGTI', 'SMR', 'QS', 'RXRX', 'RPD', 'KC', 'PLUG', 'APLD', 'BEAM', 'TXG', 'LUNR', 'TDOC', 'QBTS', 'SDGR', 'FSLY', 'OLO', 'DQ', 'QUBT', 'PL', 'TLRY', 'BBAI', 'RDW', 'SERV', 'NTLA', 'NNE', 'RCAT', 'DNA', 'RZLV', 'KULR', 'NNOX', 'CHPT', 'PACB', 'SPIR', 'LAES', 'QSI', 'VLN', 'RR', 'BKSY', 'ARQQ', 'CGC', 'ARBE', 'ACB', 'NUKK', 'QMCO', 'REKR', 'OPTT', 'NKLA', 'EDIT', 'INTZ', 'MDAI', 'SIDU', 'EKSO']
+    list_keys = ['symbol', 'shortName', 'country','industry','sector','currentPrice','marketCap', 'trailingPE', 'forwardPE', 'priceToSalesTrailing12Months', 'priceToBook', 'debtToEquity','shortRatio', 'enterpriseToRevenue', 'enterpriseToEbitda', 'beta', 'fiftyTwoWeekHigh', 'fiftyTwoWeekLow', 'targetMeanPrice', 'targetHighPrice', 'recommendationKey','returnOnEquity', 'totalRevenue', 'freeCashflow', 'totalDebt', 'earningsGrowth', 'revenueGrowth', 'grossMargins', 'ebitdaMargins', 'operatingMargins', 'profitMargins','trailingPegRatio']
     data = []
     for ticker in tickers:
         stock = yf.Ticker(ticker)
